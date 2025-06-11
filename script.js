09de7ab8af6be23da13b5d4f94684fadf1dc0fbc
adilorenzoBenefind <andrea.dilorenzo@benefindgroup.it>
2025-06-11 15:11:29 +0200

    fix value

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ const loginBtn=document.getElementById("loginBtn");
 const inputPassword=document.getElementById("inputPassword");
 const inputEmail=document.getElementById("inputEmail");
 function stampaDati(){
-    
+    console.log(inputEmail.value)
 }
-loginBtn.addEventListener("click", stampaDati());
