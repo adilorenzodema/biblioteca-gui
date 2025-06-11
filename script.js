4e3876582e615f3981e9e1e3933d0c00190f16de
LAPTOP-J687HLLB\Neri Calamai <nericalamai999@gmail.com>
2025-06-11 16:03:52 +0200

    inizio area personale

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const loginBtn=document.getElementById("loginBtn");
 const inputPassword=document.getElementById("inputPassword").value;
 const inputEmail=document.getElementById("inputEmail").value;
 function stampaDati(){
-    console.log(inputEmail.value)
+    
 }
+loginBtn.addEventListener("click", stampaDati());
