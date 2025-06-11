a85b234fe4b1c693278485d113e07a9ab8911f99
LAPTOP-J687HLLB\Neri Calamai <nericalamai999@gmail.com>
2025-06-11 14:08:25 +0200

    edit login

---
+++ script.js
@@ -0,0 +1,7 @@
+const loginBtn=document.getElementById("loginBtn");
+const inputPassword=document.getElementById("inputPassword");
+const inputEmail=document.getElementById("inputEmail");
+function stampaDati(){
+    
+}
+loginBtn.addEventListener("click", stampaDati());
