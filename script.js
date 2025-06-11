3e7e839458bf86f730b4cc1a85a6bcf5beefe875
LAPTOP-J687HLLB\Neri Calamai <nericalamai999@gmail.com>
2025-06-11 15:08:07 +0200

    edit login

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const loginBtn=document.getElementById("loginBtn");
-const inputPassword=document.getElementById("inputPassword");
-const inputEmail=document.getElementById("inputEmail");
+const inputPassword=document.getElementById("inputPassword").value;
+const inputEmail=document.getElementById("inputEmail").value;
 function stampaDati(){
-    
+    console.log(document.getElementById("inputEmail").value);
 }
 loginBtn.addEventListener("click", stampaDati());
